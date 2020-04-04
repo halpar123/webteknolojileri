867ed8fbc09c31bf405a6df42eebf7d92e076223
halpar123 <63157220+halpar123@users.noreply.github.com>
2020-04-04 18:15:02 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+<footer>
+<div class="row mavi font">
+<div class="container">
+<div class="col-lg-4 col-md-4 col-sm-4 hazirlayanlar"><h6>Hazırlayanlar</h6>
+<div class="hazirlayanlar">
+<ul>
+<li>Alper Sarper</li>
+</ul>
+</div>
+</div>
+<div class="col-lg-4 col-md-4 col-sm-4 hazirlayanlar"><h6>Kullanılan Diller</h6>
+<ul>
+<li>PHP</li>
+<li>HTML</li>
+<li>CSS</li>
+<li>JAVASCRİPT</li>
+<li>JQUERY</li>
+</ul>
+</div>
+ <div class="col-lg-4 col-md-4 col-sm-4 neyapiyoriz"><h6>Ne Yapıyoruz</h6>
+ <ul>
+ <li>Boostrap Kullanımı</li>
+<li>Dinamik Web Sitesi</li>
+ </ul>
+ <div class="iletisim">
+ <a href="https://www.facebook.com/Alper.Sarper" target="_blank" class="fa fa-facebook"></a> <a href="https://twitter.com/halparsarper" target="_blank" class="fa fa-twitter"></a>
+ </div>
+ </div>
+</div>
+</div>
+</footer>
+<script src="js/jquery-1.12.3.min.js"></script>
+<script src="js/bootstrap.min.js" ></script>
+<script src="ckeditor/ckeditor.js" type="text/javascript" language="javascript"></script>
+
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="css/bootstrap.min.css" rel="stylesheet" />
+<link href="css/style.css" rel="stylesheet" />
+<link rel="stylesheet" href="font-awesome-4.6.2/css/font-awesome.min.css"/>
+<title>Alper Sarper</title>
+</head>
+
+<body>
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php">Alper Sarper Blog</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="wobble-top"><a href="index.php">Ben kimim?<span class="sr-only">(current)</span></a></li>
+        <li class="wobble-top"><a href="egitim.php">Eğitim</a></li>
+        <li class="wobble-top"><a href="sehrim.php">Şehrim</a></li>
+        <li class="wobble-top"><a href="mirasimiz.php">Mirasımız</a></li>
+        <li class="wobble-top"><a href="iletisim.php">İletişim</a></li>
+        <li class="wobble-top"><a href="logout.php">Çıkış Yap</a></li>
+        
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
